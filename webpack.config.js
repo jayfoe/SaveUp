9a6ad14c7204ca41de7bd26ea439b2133c0c8328
Holocraft <perrin1057@gmail.com>
2017-04-04 23:10:47 -0700

    fixed webpack issue that was preventing the server from loading css

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -9,13 +10,20 @@ module.exports = {
   module: {
     loaders: [
       {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
-      }
+      },
     ]
   },
   resolve: {
-    extensions: ['.js', '.json']
-  }
+    extensions: ['.js', '.json', '.css']
+  },
+  plugins: [
+    new ExtractTextPlugin("app.css")
+  ]
 };
